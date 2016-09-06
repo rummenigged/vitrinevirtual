44b26f5ca6a2800fc2fe14b80870bd6d91031cbd
rummenigged <rummenigge.duarte@gmail.com>
2016-09-06 20:25:15 -0300

    Tela de Login e Tela Principal do cms

---
+++ cms/html/js/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    if( window.width > 1023){
+//     $("#autenticacao button").removeClass("s10 offset-s1");   
+     $("#autenticacao button").addClass("s6 offset-s3");
+        
+    }
+    
+    $("#autenticacao").on('submit', function(){
+        
+    });
+});
---
+++ cms/html/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $('#menu-sandwich').on('click', function(){
+//        $('.side-menu').show();
+//        $('.side-menu').animate({left : 0}, 500);
+    });
+});
