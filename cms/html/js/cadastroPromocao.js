dc239233b20c327e0280fcc5ef65e68276f93024
rummenigged <rummenigge.duarte@gmail.com>
2016-09-09 21:33:52 -0300

    Cadastrar Promoção com o range de dias incompleto - Menu lateral fixo em tela desktop com icones de drop menu não formatados

---
+++ cms/html/js/cadastroPromocao.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    //Range para escolher intervalo de dias da duração da promoção
+    
+//    noUiSlider.create($('#range-slider'),{
+//        start: [1,10],
+//        connect: true,
+//        step: 1,
+//        range: {'min': 1 , 'max': 31},
+//        format: wNumb({decimals: 0})
+//    });
+    
+//        $('#range-slider').slider({
+//            range: true,
+//            min: 1,
+//            max: 31,
+//            values: [1, 10]
+//        });
+//   
+    
+    if($(window).width > 600 && $(window).width <= 900){
+           
+    }
+    
+});
--- cms/html/js/main.js
+++ cms/html/js/main.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
 
+      $(document).ready(function() {
+        $('select').material_select();
+      });
+    
+    if(screen.width > 900){
+        $('.side-menu').show();
+        $('.side-menu').css({left : 0});
+        
+        $('.logo').show();
+    }
     $('#menu-sandwich').on('click', function(){
-//        $('.side-menu').show();
-//        $('.side-menu').animate({left : 0}, 500);
+        
+        $('.side-menu').show();
+        $('.side-menu').animate({left : 0}, 500);
+        $('.content').animate({left : "50%"}, 500);
+        
+    });
+    
+    $('#menu-close').on('click', function(){
+        
+        $('.side-menu').animate({left : "-50%"}, 500);
+        $('.content').animate({left : 0},500,function(){
+            $('.side-menu').hide()}
+        );
+        
+    });
+    
+    $('#menu-close').on('hover', function(){
+        
+    });
+    
+    $('#menu-sandwich').on('hover', function(){
+        
+    });
+    
+    $('.drop-menu').on('click', function(){
+        
     });
 });
